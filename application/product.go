df61adf3906f994838bdd613a939d1b1d8d762d5
agftech <agiraof@gmail.com>
2021-05-01 10:35:25 -0700

    :package: feat: implement enable function

---
+++ application/product.go
@@ -0,0 +1,29 @@
+package application
+
+import (
+	"errors"
+)
+
+type ProductInterface interface {
+	Enable() error
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+	return errors.New("the price must be greater than zero to enable the product")
+}
