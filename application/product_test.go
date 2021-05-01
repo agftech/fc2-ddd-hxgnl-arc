1fab29cc7a2dabeed98a2cc13a529edc7ae14bc7
agftech <agiraof@gmail.com>
2021-05-01 11:11:29 -0700

    :white_check_mark: test: add function test enable

---
+++ application/product_test.go
@@ -0,0 +1,22 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/agftech/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
