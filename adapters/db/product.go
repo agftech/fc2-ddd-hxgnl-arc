a58bbb7f5b5d5c104d332de8e8032a351bd41731
agftech <agiraof@gmail.com>
2021-05-05 15:31:53 -0700

    :package: feat: create db adapter

---
+++ adapters/db/product.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/agftech/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
