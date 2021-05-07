f729a0459bb97dba96ef529ad6674bcd3c4af5fd
agftech <agiraof@gmail.com>
2021-05-07 14:45:39 -0700

    :package: feat: add data on the db adapter

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+
+	db2 "github.com/agftech/go-hexagonal/adapters/db"
+	"github.com/agftech/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, _ := sql.Open("sqlite3", "db.sqlite")
+	productDbAdapter := db2.NewProductDb(db)
+	productService := application.NewProductService(productDbAdapter)
+	product, _ := productService.Create("Product Exemplo", 30)
+
+	productService.Enable(product)
+
+}
