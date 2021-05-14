c4a116ec1ce5ffada0c727a48dcdf49a311b44c9
agftech <agiraof@gmail.com>
2021-05-14 12:28:15 -0700

    :package: feat: create web server

---
+++ adapters/web/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/agftech/go-hexagonal/application"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":9000",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
