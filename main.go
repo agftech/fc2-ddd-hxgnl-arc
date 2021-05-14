474a5f1fc527201a41a3a56905907b77226cdb85
agftech <agiraof@gmail.com>
2021-05-13 17:33:22 -0700

    :package: feat: implement cli with cobra

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/agftech/go-hexagonal/adapters/db"
-	"github.com/agftech/go-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/agftech/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemplo", 30)
-
-	productService.Enable(product)
-
+	cmd.Execute()
 }
